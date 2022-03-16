b91ef3332bfa63c34124084433862257148cf4f9
Hillus Hilbert <hillus.hilbert@codding.com.br>
2022-03-16 19:59:52 -0300

    fix: tratamento de indice invalido

--- sge/app/Application/Path.php
+++ sge/app/Application/Path.php
@@ -29,7 +29,7 @@ class Path
             $dados = $this->loadMatriz();
         }
 
-        return $dados[$this->node1->name][$this->node2->name];
+        return $dados[$this->node1->name][$this->node2->name] ?? 0;
     }
 
     private function loadMatriz()
